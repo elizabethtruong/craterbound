d98ac09a73c5ae9e34ecbf2c82836ed2131144b8
Elizabeth Truong <eat0031@auburn.edu>
2022-04-04 03:37:02 -0500

    tweaked animation, fixed physics in tutorial

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,56 +7,59 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D body;
     private Animator anim;
     private BoxCollider2D boxCollider;
+    private SpriteRenderer sprite;
     //private bool grounded;
 
-    private void Awake()
+    private float horizontalInput = 0f;
+
+    private void Start()
     {
-        // Grab references for rigidbody and animator from object
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
         boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
+        horizontalInput = Input.GetAxisRaw("Horizontal");
         body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
-        // Flip player when moving left and right
-        if (horizontalInput > 0.01f)
+        if (Input.GetButtonDown("Jump"))
         {
-            transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+            body.velocity = new Vector2(body.velocity.x, 14f);
+            anim.SetTrigger("jump");
         }
 
-        else if (horizontalInput < -0.01f)
-        {
-            transform.localScale = new Vector3(-0.25f, 0.25f, 0.25f);
-        }
-
-        if (Input.GetKey(KeyCode.Space) && isGrounded())
-        {
-            Jump();
-        }
-
+        UpdateAnimationState();
         // Set animator parameters
-        anim.SetBool("walk", horizontalInput != 0);
         anim.SetBool("grounded", isGrounded());
-    }
-
-    private void Jump()
-    {
-        body.velocity = new Vector2(body.velocity.x, speed);
-        anim.SetTrigger("jump");
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision) 
-    {
-
     }
 
     private bool isGrounded()
     {
-        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
-        return raycastHit.collider != null;
+        return Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, .1f, groundLayer);
+    }
+
+    private void UpdateAnimationState()
+    {
+
+        // Flip player when moving left and right
+        if (horizontalInput > 0f)
+        {
+            anim.SetBool("walk", true);
+            transform.localScale = new Vector3(.1f, .1f, .1f);
+        }
+
+        else if (horizontalInput < 0f)
+        {
+            anim.SetBool("walk", true);
+            transform.localScale = new Vector3(-.1f, .1f, .1f);
+        }
+
+        else
+        {
+            anim.SetBool("walk", false);
+        }
     }
 }
